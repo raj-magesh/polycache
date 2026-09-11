048d1fbb401dce2af2fd2190855184d22364d9ea
Raj Magesh Gauthaman <git@raj-magesh.org>
2026-09-10 22:27:59 -0400

    add support for .fif files split according to BIDS convention

--- src/polycache/_handlers/mne_epochs.py
+++ src/polycache/_handlers/mne_epochs.py
@@ -8,9 +8,16 @@ if TYPE_CHECKING:
     from pathlib import Path
 
 
-def save(result: mne.BaseEpochs, /, *, filepath: Path, **kwargs: Any) -> None:  # noqa: ANN401
+def save(result: mne.BaseEpochs, /, *, filepath: Path, **kwargs: Any) -> None:  # ruff: ignore[any-type]
     result.save(filepath, **({"split_naming": "bids"} | kwargs))
 
 
-def load(filepath: Path, /, **kwargs: Any) -> mne.BaseEpochs:  # noqa: ANN401
-    return mne.read_epochs(filepath, **({"preload": False} | kwargs))
+def load(filepath: Path, /, **kwargs: Any) -> mne.BaseEpochs:  # ruff: ignore[any-type]
+    try:
+        return mne.read_epochs(filepath, **({"preload": False} | kwargs))
+    except FileNotFoundError:
+        split = filepath.name.split("_")
+        return mne.read_epochs(
+            filepath.parent / "_".join([*split[:-1], "split-01", split[-1]]),
+            **kwargs,
+        )
--- src/polycache/_handlers/mne_io_raw.py
+++ src/polycache/_handlers/mne_io_raw.py
@@ -8,9 +8,16 @@ if TYPE_CHECKING:
     from pathlib import Path
 
 
-def save(result: mne.io.BaseRaw, /, *, filepath: Path, **kwargs: Any) -> None:  # noqa: ANN401
+def save(result: mne.io.BaseRaw, /, *, filepath: Path, **kwargs: Any) -> None:  # ruff: ignore[any-type]
     result.save(filepath, **({"split_naming": "bids"} | kwargs))
 
 
-def load(filepath: Path, /, **kwargs: Any) -> mne.io.BaseRaw:  # noqa: ANN401
-    return mne.io.read_raw_fif(filepath, **kwargs)
+def load(filepath: Path, /, **kwargs: Any) -> mne.io.BaseRaw:  # ruff: ignore[any-type]
+    try:
+        return mne.io.read_raw_fif(filepath, **kwargs)
+    except FileNotFoundError:
+        split = filepath.name.split("_")
+        return mne.io.read_raw_fif(
+            filepath.parent / "_".join([*split[:-1], "split-01", split[-1]]),
+            **kwargs,
+        )
